bf798a4913ce08685eed23fd2bfd1cf8eb81d8a6
joerow <joerow@gmail.com>
2023-10-20 16:56:15 +0800

    add loader

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ const city = document.getElementById("city");
 const currentCondition = document.getElementById("currentCondition");
 const temperature = document.getElementById("temperature");
 const degsym = "\xB0";
+const loader = document.getElementById("loader");
 
 async function getData(location) {
   try {
@@ -19,6 +20,7 @@ async function getData(location) {
       { mode: "cors" }
     );
     const weatherData = await response.json();
+    loader.style.display = await "none";
     const searchedLocation = new Location(
       weatherData.location.name,
       weatherData.current.condition.text,
@@ -34,6 +36,9 @@ async function getData(location) {
       "Currently: " + weatherData.current.condition.text;
     temperature.textContent =
       "Temperature: " + weatherData.current.temp_c + degsym + "C";
+    city.style.display = await "block";
+    currentCondition.style.display = await "block";
+    temperature.style.display = await "block";
     return searchedLocation;
   } catch (error) {
     console.log(error);
@@ -49,13 +54,18 @@ async function getData(location) {
 
 async function processData(location) {
   let somedata = getData(location);
+
   return somedata;
 }
 
 searchBox.addEventListener("keyup", ({ key }) => {
   if (key === "Enter") {
     console.log("enter pressed");
+    loader.style.display = "block";
     console.log(searchBox.value);
+    city.style.display = "none";
+    currentCondition.style.display = "none";
+    temperature.style.display = "none";
     let x = processData(searchBox.value);
     console.log(x);
     searchBox.value = "";
